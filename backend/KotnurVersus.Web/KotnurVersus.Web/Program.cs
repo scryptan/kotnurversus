675090b907f8505a0985dd79c1e3b266fd8ba56f
scryptan <scryptan@gmail.com>
2023-10-04 17:27:57 +0500

    add base auto DI

--- backend/KotnurVersus.Web/KotnurVersus.Web/Program.cs
+++ backend/KotnurVersus.Web/KotnurVersus.Web/Program.cs
@@ -1,8 +1,20 @@
+using System.Reflection;
+using KotnurVersus.Web.Helpers;
+using KotnurVersus.Web.Helpers.DI;
+using Vostok.Logging.Console;
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.WebHost.UseUrls("https://0.0.0.0/");
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var assemblyHelper = new AssemblyHelpers();
+var applicationAssemblies = new List<Assembly>(assemblyHelper.Assemblies);
+builder.Services.AddApplicationServices(new ConsoleLog(),
+    applicationAssemblies.Distinct().ToArray(),
+    assemblyHelper.ExcludeDiTypes.ToHashSet());
+builder.Services.AddLazy();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
