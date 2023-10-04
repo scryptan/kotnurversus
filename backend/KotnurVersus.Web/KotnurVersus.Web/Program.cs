56dd0a8fc9f3cef634ef0f6989b045246219c909
scryptan <scryptan@gmail.com>
2023-10-04 19:11:08 +0500

    setup logging and hosting

--- backend/KotnurVersus.Web/KotnurVersus.Web/Program.cs
+++ backend/KotnurVersus.Web/KotnurVersus.Web/Program.cs
@@ -1,12 +1,48 @@
 using System.Reflection;
+using KotnurVersus.Web.Configuration;
 using KotnurVersus.Web.Helpers;
 using KotnurVersus.Web.Helpers.DI;
+using Vostok.Hosting;
+using Vostok.Hosting.Abstractions;
+using Vostok.Hosting.Abstractions.Helpers;
+using Vostok.Hosting.AspNetCore;
+using Vostok.Hosting.Setup;
+using Vostok.Logging.Abstractions;
 using Vostok.Logging.Console;
+using Vostok.Logging.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
-builder.WebHost.UseUrls("https://0.0.0.0/");
 builder.Services.AddControllers();
+
+builder.UseVostokHosting(
+    environmentBuilder =>
+    {
+        environmentBuilder.SetupHostExtensions(
+            extensions =>
+            {
+                var vostokHostShutdown = new VostokHostShutdown(new CancellationTokenSource());
+                extensions.Add(vostokHostShutdown);
+                extensions.Add(typeof(IVostokHostShutdown), vostokHostShutdown);
+            });
+        environmentBuilder.SetupLog(x => x.SetupConsoleLog());
+        environmentBuilder.SetupConfiguration(
+            c => { c.SetupSourceFor<WebSecrets>(); });
+
+        environmentBuilder.SetPort(4000);
+
+        var appName = Assembly.GetEntryAssembly()?.GetName().Name ?? "unknown";
+        environmentBuilder
+            .DisableServiceBeacon()
+            .SetupApplicationIdentity(
+                identityBuilder => identityBuilder
+                    .SetProject("KotnurVersus")
+                    .SetApplication(appName)
+                    .SetEnvironment("local")
+                    .SetInstance(Environment.GetEnvironmentVariable("HOSTNAME") ?? $"local_{appName}"));
+    });
+builder.Services.AddSingleton<ILog>(x =>
+    x.GetRequiredService<IVostokHostingEnvironment>().Log.WithAllFlowingContextProperties());
 
 var assemblyHelper = new AssemblyHelpers();
 var applicationAssemblies = new List<Assembly>(assemblyHelper.Assemblies);
