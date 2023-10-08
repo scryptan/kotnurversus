8c814ed0c093ed8e623a9e769bee718021b81956
Nikita <todti@jinteki.dev>
2023-10-08 18:26:52 +0500

    init frontend project using template

---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint ./src',
+      },
+    }),
+  ],
+});
