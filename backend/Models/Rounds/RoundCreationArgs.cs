11b73015fbd394b838e2e9edeb8cc15aaf93c4da
scryptan <scryptan@gmail.com>
2023-11-29 16:15:05 +0500

    try start a game

--- backend/Models/Rounds/RoundCreationArgs.cs
+++ backend/Models/Rounds/RoundCreationArgs.cs
@@ -1,8 +1,12 @@
+using Models.Specifications;
+
 namespace Models.Rounds;
 
 public class RoundCreationArgs : EntityCreationArgs
 {
     public Guid GameId { get; set; }
     public Guid? NextRoundId { get; set; }
+    public List<Participant>? Participants { get; set; }
+    public Specification Specification { get; set; } = null!;
     public int Order { get; set; }
 }
