9e21e4af32269f061e263b0abf5e06bc8719cf54
scryptan <scryptan@gmail.com>
2023-12-02 14:53:16 +0500

    rm flags from db

--- backend/Db/Dbo/Games/GameDbo.cs
+++ backend/Db/Dbo/Games/GameDbo.cs
@@ -32,10 +32,4 @@ public class GameDbo : Dbo
 
     [Column("state")]
     public GameState State { get; set; }
-
-    [Column("cats_in_the_bag")]
-    public bool CatsInTheBag { get; set; }
-
-    [Column("without_challenges_repeat_in_final")]
-    public bool WithoutChallengesRepeatInFinal { get; set; }
 }
