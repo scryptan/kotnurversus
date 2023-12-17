9fe15d3122dd801879c62c599a96fc74aaf6d748
scryptan <scryptan@gmail.com>
2023-12-17 15:35:08 +0500

    extend users dbo

--- backend/Db/Dbo/Users/UserDbo.cs
+++ backend/Db/Dbo/Users/UserDbo.cs
@@ -7,4 +7,10 @@ public class UserDbo : Dbo
 {
     [Column("email")] 
     public string Email { get; set; } = null!;
+    
+    [Column("password_hash")] 
+    public string PasswordHash { get; set; } = null!;
+
+    [Column("is_authorized")]
+    public bool IsAuthorized { get; set; } = false;
 }
